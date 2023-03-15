160f8b5b91a6756f9e475843862aaa4b45e2874d
William Freire de Souza e Silva <williamfreire.silva@outlook.com>
2023-03-15 13:25:05 +0000

    Inserted comments.

--- main.py
+++ main.py
@@ -1,23 +1,25 @@
 import requests
 
-def get_news(country,api_key='83bac02b80354202bb771a814fa9ed67'):
+# create a function that receives a coutry's acronym, not case sensitive, and your API key.
+# API key should be insertid in the function as default
+# get your API key at: https://newsapi.org
+def get_news(country,api_key='<yout API key from: https://newsapi.org'):
   url = f'https://newsapi.org/v2/top-headlines?country={country}&apiKey={api_key}'
   r = requests.get(url)
   content = r.json()
   articles = content['articles']
   results = []
   
-
+  # loop through each article and append them to results[]
   for article in articles:
     results.append(f"\nTITLE\n {article['title']} \n\nDESCRIPTION\n {article['description']} \n" + len(article['description'])*'_')
-    '''
-    for result in results:
-      return result
-    ''' 
+
+  # if you return results, you won't get your escape characters to work
+  # while loop to print each result individualy to make use of escape characters for aesthetics
   count = 0
   while count < (len(results) - 1):
     print(results[count])
     count = count + 1
     
-
+# summon function for USA headlines
 print(get_news(country='us'))
